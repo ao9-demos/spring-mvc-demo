05c4408469ad0171f2d3e5731550ab52c88f1615
Ao Dong <aodong99@gmail.com>
2020-01-13 20:38:12 -0500

    "update"

---
+++ src/main/java/io/ao9/hibernatedemo/CreateStudent.java
@@ -0,0 +1,40 @@
+package io.ao9.hibernatedemo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import io.ao9.hibernatedemo.entity.Student;
+
+public class CreateStudent{
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration()
+                                    .configure("hibernate.cfg.xml")
+                                    .addAnnotatedClass(Student.class)
+                                    .buildSessionFactory();
+        
+        Session session = factory.getCurrentSession();
+
+        try {
+            System.out.println("Creating a new student");
+            Student theStudent = new Student("John","Smith","jsmith@123.com");
+
+            System.out.println("begin transaction");
+            session.beginTransaction();
+
+            System.out.println("saving the student");
+            session.save(theStudent);
+
+            System.out.println("commiting...");
+            session.getTransaction().commit();
+            System.out.println("done");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            factory.close();
+        }
+
+    }
+
+}
--- src/main/java/io/ao9/hibernatedemo/entity/Student.java
+++ src/main/java/io/ao9/hibernatedemo/entity/Student.java
@@ -13,7 +13,7 @@ public class Student{
     private int id;
     
     @Column(name = "first_name")
-    private String firstName
+    private String firstName;
     
     @Column(name = "last_name")
     private String lastName;
@@ -29,5 +29,47 @@ public class Student{
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Student: {" +
+            " id='" + getId() + "'" +
+            ", firstName='" + getFirstName() + "'" +
+            ", lastName='" + getLastName() + "'" +
+            ", email='" + getEmail() + "'" +
+            "}";
     }
 }
