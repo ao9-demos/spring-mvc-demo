324b938196d32cb2bf38929c07e5f2b586da1468
Ao Dong <aodong99@gmail.com>
2020-04-10 19:24:02 -0400

    update

--- src/main/java/io/ao9/jdbc/TestJDBC.java
+++ src/main/java/io/ao9/jdbc/TestJDBC.java
@@ -6,8 +6,8 @@ import java.sql.DriverManager;
 public class TestJDBC{
     public static void main (String[] args) {
         String jdbcUrl = "jdbc:mysql://34.74.126.132:3306/hb-05-many-to-many?useSSL=false&serverTimezone=UTC";
-		String user = "hbstudent";
-		String pass = "hbstudent";
+		String user = "springstudent";
+		String pass = "springstudent";
         
         try {
             System.out.println("connecting: "+jdbcUrl);
